ae21720554b4985446b1ffc35a2f7bc241cb54f5
Gui ximiti <gui.m.schmidt@gmail.com>
2022-06-10 00:05:02 -0300

    update images
    update images resolutions & positions

--- script.js
+++ script.js
@@ -43,8 +43,8 @@ document.addEventListener('mousemove', parallax);
 function parallax(e){
   this.querySelectorAll('.layer').forEach(layer =>{
     var speed = layer.getAttribute('data-speed');
-    var x     = (window.innerWidth - e.pageX * speed)/200;
-    var y     = (window.innerWidth - e.pageY * speed)/200;
+    var x     = (window.innerWidth - e.pageX * speed)/300;
+    var y     = (window.innerWidth - e.pageY * speed)/300;
     layer.style.transform = `translateX(${x}px) translateY(${y}px)`
 
   });
